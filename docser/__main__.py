353a4118ee530ae1e5fb39e196bd0563cf0104cc
chrisbrake <chris.brake@gmail.com>
2018-11-01 23:09:42 -0600

    Made an API

--- docser/__main__.py
+++ docser/__main__.py
@@ -1,11 +1,34 @@
 import responder
+from marshmallow import Schema, fields
 
-api = responder.API()
+from ._version import get_versions
+
+api = responder.API(title='Documentation Server',
+                    version=get_versions()['version'],
+                    openapi='3.0',
+                    docs_route='/api/docs')
+fake_db = [{'name': 'example'}]
 
 
-@api.route("/{greeting}")
-async def greet_world(req, resp, *, greeting):
-    resp.text = f"{greeting}, world!"
+@api.schema("Project")
+class ProjectSchema(Schema):
+    name = fields.Str()
+
+
+@api.route("/api/projects")
+async def projects(req, resp):
+    """
+    The end point for all the projects hosted here
+    ---
+    get:
+        description: Get all the stored projects
+        responses:
+            200:
+                description: All the stored projects
+                schema:
+                    $ref = "#/components/schemas/Project"
+    """
+    resp.media = [ProjectSchema().dump(i) for i in fake_db]
 
 if __name__ == '__main__':
-    api.run(address='0.0.0.0', port=80)
+    api.run(address='0.0.0.0', port=8080)
